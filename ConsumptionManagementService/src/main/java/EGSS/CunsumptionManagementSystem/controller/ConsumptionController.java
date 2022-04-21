63aa6f0e702c3351549c8fca4ae403188585494e
SiriwardanaT <thariharshana123@gmail.com>
2022-04-22 03:51:18 +0530

    modal created and controller started

--- ConsumptionManagementService/src/main/java/EGSS/CunsumptionManagementSystem/controller/ConsumptionController.java
+++ ConsumptionManagementService/src/main/java/EGSS/CunsumptionManagementSystem/controller/ConsumptionController.java
@@ -1,12 +1,15 @@
 package EGSS.CunsumptionManagementSystem.controller;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import EGSS.CunsumptionManagementSystem.modal.Consumption;
 
-@Path("myresource")
+
+@Path("admin")
 public class ConsumptionController {
 	
     @GET
@@ -14,4 +17,14 @@ public class ConsumptionController {
     public String getIt() {
         return "Consumption Management system";
     }
+    
+    //post:
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public String AddNewConsupmtion(Consumption consumption) {
+    	  System.out.println(consumption.getId());
+    	  return consumption.getId();
+    }
+    
+   
 }
