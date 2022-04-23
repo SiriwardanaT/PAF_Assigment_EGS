52a83dae3eec30bca9e379030d7bc3656fbfe10e
PiratheepThomas <IT19963334@my.sliit.lk>
2022-04-24 00:28:07 +0530

    Updates

--- ComplaintManagementService/src/main/java/EGSS/ComplaintManagementSystem/controller/ComplaintController.java
+++ ComplaintManagementService/src/main/java/EGSS/ComplaintManagementSystem/controller/ComplaintController.java
@@ -1,13 +1,17 @@
 package EGSS.ComplaintManagementSystem.controller;
 
+import java.io.ObjectInputFilter.Status;
 import java.sql.SQLException;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import EGSS.ComplaintManagementSystem.dao.ComplaintService;
 import EGSS.ComplaintManagementSystem.modal.Complaint;
@@ -15,28 +19,46 @@ import EGSS.ComplaintManagementSystem.modal.Complaint;
 
 
 
-@Path("customer")
 
+@Path("complaint")
 public class ComplaintController {
 	
-	    
-	@POST
-    @Produces(MediaType.APPLICATION_JSON)
-        public String AddNewComplaint(Complaint complaint) throws ClassNotFoundException, SQLException {
-        	if(complaint != null) {
-        		return ComplaintService.AddComplaint(complaint);
-        	}
-        	else {
-        		return "Something went wrong !";
-        	}
-        	
-        }
+ 	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getComplaint() {
+		return "Hello";
+	}
 
-	@Path("{Id}")
-    @GET
-    public String SearchConsumption(@PathParam("Id") String acc) throws ClassNotFoundException, SQLException {
-    	
-    	return ComplaintService.SearchComplaintById(Id);
-    }
+//	@POST
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public String AddNewConsupmtion(Complaint complaint) throws ClassNotFoundException, SQLException {
+//    	if(complaint != null) {
+//    		return ComplaintService.AddComplaint(complaint);
+//    	}
+//    	else {
+//    		return "Something went wrong !";
+//    	}
+//    	
+//    }
 	
+ 	
+    @GET
+    public String SearchComplaint(@PathParam("Id") String Id) throws ClassNotFoundException, SQLException {
+    	
+    	return ComplaintService.SearchComplaint(Id);
+    }
+    
+    
+    @DELETE
+    public String DeleteRecord(@PathParam("Id") String Id) throws ClassNotFoundException, SQLException {
+    	   return ComplaintService.DeleteComplaint(Id);
+    }
+    
+    
+    @PUT
+    public int updateRecord(Complaint complaint, @PathParam("Id") int Id) {
+    	  System.out.println(complaint.getId());
+    	  return Id;
+    }
+
 }
--- ComplaintManagementService/src/main/java/EGSS/ComplaintManagementSystem/dao/ComplaintService.java
+++ ComplaintManagementService/src/main/java/EGSS/ComplaintManagementSystem/dao/ComplaintService.java
@@ -5,27 +5,26 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import EGSS.ComplaintManagementSystem.utils.dbConnection;
+
 import EGSS.ComplaintManagementSystem.modal.Complaint;
 import EGSS.ComplaintManagementSystem.utils.Constant;
-
-
+import EGSS.ComplaintManagementSystem.utils.dbConnection;
 
 public class ComplaintService {
 	
 	public Connection con;
 	ComplaintService() throws ClassNotFoundException, SQLException{
 		Connection con = dbConnection.getdbConnection();
-	}
 
+}
+	// Add Complaint
 	
-	public static String AddComplaint(Complaint complaint)throws ClassNotFoundException, SQLException {
+	public static String AddComplaint(Complaint complaint) throws ClassNotFoundException, SQLException {
 		String output = "";
-		
 		Connection con = dbConnection.getdbConnection();
 		if(con == null) {
 			return Constant.ConnectionERR;
-			}
+		}
 		else {
 			PreparedStatement preparedStatement =  con.prepareStatement("insert into Inquery (id,subject,content,uid,status,createBy,createDate,modifiedBy,modifiedDate)  values(?,?,?,?,?,?,?,?,?,?,?)");
 			preparedStatement.setString(1,complaint.getId());
@@ -46,23 +45,27 @@ public class ComplaintService {
 			}
 			else {
 				return output  = "Unable to submit this complaint";
-			}}
-		}		
-			
-	public static String SearchComplaintById(String Id)throws ClassNotFoundException, SQLException{
+		
+		}
+		}
+}
+	public static String SearchComplaint(String Id) throws ClassNotFoundException, SQLException {
 		Connection con = dbConnection.getdbConnection();
-		PreparedStatement preparedStatement =  con.prepareStatement("select * from Inquery where Id like ?");
+		PreparedStatement preparedStatement =  con.prepareStatement("select * from Inquery where id like ?");
 		preparedStatement.setString(1, Id);
 		ResultSet rs =  preparedStatement.executeQuery();
-		String output = "<h3>Complaint" + Id + "</h3>";
+		String output = "<h3>Complaint " + Id + "</h3>";
 		output += "<table border='1'>";
 		output += "<tr>\r\n"
 	        		+ "    <th>ComplaintID</th>\r\n"
 	        		+ "    <th>Subject</th>\r\n"
-	        		+ "    <th>Conetent</th>\r\n"
+	        		+ "    <th>Content Date</th>\r\n"
+	        		+ "    <th>User ID</th>\r\n"
 	        		+ "    <th>Status</th>\r\n"
 	        		+ "    <th>Created By</th>\r\n"
 	        		+ "    <th>Created Date</th>\r\n"
+	        		+ "    <th>Modified By</th>\r\n"
+	        		+ "    <th>Modified Date</th>\r\n"
 	        		+ "  </tr>";
 		while(rs.next()) {
 			output += "<tr>";
@@ -72,12 +75,46 @@ public class ComplaintService {
 			output += "<th>"+rs.getString(4) +"</th>";
 			output += "<th>"+rs.getString(5) +"</th>";
 			output += "<th>"+rs.getString(6) +"</th>";
+			output += "<th>"+rs.getString(7) +"</th>";
+			output += "<th>"+rs.getString(8) +"</th>";
+			output += "<th>"+rs.getString(9) +"</th>";
 			output += "</tr>";
-	}
-	
+		}
 		output += "</table>";
 		return output;
 	}
-		
 	
+	public static String DeleteComplaint(String Id) throws ClassNotFoundException, SQLException {
+		Connection con = dbConnection.getdbConnection();
+		if(con == null) {
+			return Constant.ConnectionERR;
+		}
+		else {
+			PreparedStatement preparedStatement =  con.prepareStatement("delete from Inquery where id = ? ");
+			preparedStatement.setString(1, Id);
+			int res = preparedStatement.executeUpdate();
+			System.out.println(res);
+			String output = "";
+			if(res == 1) {
+				
+				return output = "Deleted Successfully";
+			}
+			else {
+				return output = "Not Deleted";
+			}
+		}
+	}
+
+//	public static String updateComplaint(Complaint complaint, String Id) throws ClassNotFoundException, SQLException {
+//		Connection con = dbConnection.getdbConnection();
+//		if(con == null) {
+//			return Constant.ConnectionERR;
+//		}
+//		else {
+//			PreparedStatement preparedStatement =  con.prepareStatement("update consumption set  ");
+//			
+//		}
+//	}
+
+
 }
