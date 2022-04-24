335a43890e8bc4e613162ecac126ed3e2744fae0
tharukshi724 <tharukshiw@gmail.com>
2022-04-24 13:44:26 +0530

    add some changes to queries

--- CustomerManagementService/src/main/java/EGSS/CustomerManagementService/constants/CustomerConstants.java
+++ CustomerManagementService/src/main/java/EGSS/CustomerManagementService/constants/CustomerConstants.java
@@ -2,13 +2,13 @@ package EGSS.CustomerManagementService.constants;
 
 public class CustomerConstants {
 	
-	public static String ADDTOCUSTOMER = "insert  into  customer(id,firstName,lastName,NIC,email,Street,state,postalCode,status,createdBy,createdDate,modifiedBy,modifiedDate) values (?,?,?,?,?,?,?,?,?,?,?,?,?)";
-    public static String VIEWCUSTOMER = "select firstName, lastName,NIC,email,Street, state, postalCode * from customer";
-    public static String GETONECUSTOMER ="select * from customer where id=?";
-    public static String UPDATECUSTOMER ="update customer set firstName= ?,lastName=?,NIC=?,email=?,Street=?,state=?,postalCode=?,status=? where id=?";
+	public static String ADDTOCUSTOMER = "insert  into  customer(id,firstName,lastName,nic,email,street,state,postalCode,status,createBy,createDate,modifiedBy,modifiedDate,role) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+    public static String VIEWCUSTOMER = "select firstName, lastName,nic,email,street, state, postalCode , status  from powergriddb.customer";
+    public static String GETONECUSTOMER ="select * from powergriddb.customer where id=?";
+    public static String UPDATECUSTOMER ="update customer set firstName= ?,lastName=?,nic=?,email=?,street=?,state=?,postalCode=?,status=? where id=?";
     public static String DELETECUSTOMER = "delete from customer where id=?";
-    public static String GETAUTHDETAILS ="select id,role from customer order by id desc limit 1 ";
-    public static String INSERTAUTHDATA ="insert into user(uId,id,role,password)values(?,?,?,?)";
+    public static String GETAUTHDETAILS ="select id  from customer order by id desc limit 1 ";
+    public static String INSERTAUTHDATA ="insert into user(uid,password,id)values(?,?,?)";
     public static String LOGUSER = "select c.email,u.password,u.role from user u , customer c where c.email=? and u.password=?";
     
     public static int INDEX_ONE  =  1;
@@ -24,5 +24,7 @@ public class CustomerConstants {
 	public static int INDEX_ELEVEN = 11;
 	public static int INDEX_TWELEVE = 12;
 	public static int INDEX_THIRTEEN = 13;
+	public static int INDEX_FOURTEEN = 14;
+	public static int INDEX_FIFTEEN = 15;
 
 }
